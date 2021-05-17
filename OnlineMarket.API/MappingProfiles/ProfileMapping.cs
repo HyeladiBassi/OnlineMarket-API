f3f4edc29264c8c950cb2182b205459bbabe47f0
HyeladiBassi <bassihyeladi@gmail.com>
2021-05-17 17:05:55 +0300

    Added new mapping scheme

--- OnlineMarket.API/MappingProfiles/ProfileMapping.cs
+++ OnlineMarket.API/MappingProfiles/ProfileMapping.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using AutoMapper;
 using OnlineMarket.DataTransferObjects.Authentication;
 using OnlineMarket.DataTransferObjects.Deliver;
+using OnlineMarket.DataTransferObjects.Media;
 using OnlineMarket.DataTransferObjects.Product;
 using OnlineMarket.DataTransferObjects.ProductReview;
 using OnlineMarket.DataTransferObjects.SystemUser;
@@ -14,15 +15,14 @@ namespace OnlineMarket.API.MappingProfiles
     {
         public ProfileMapping()
         {
-            CreateMap<BuyerSignUpDto, SystemUser>();
-            CreateMap<ProductCreateDto, Product>();
-            CreateMap<DeliveryCreateDto, Delivery>();
-            CreateMap<TransactionCreateDto, Transaction>();
-            CreateMap<ProductReviewCreateDto, ProductReview>();
 
+            #region Authentication
+            CreateMap<BuyerSignUpDto, SystemUser>();
             CreateMap<SystemUser, SystemUserViewDto>();
-            CreateMap<ProductReview, ProductReviewViewDto>()
-                .ForMember(x => x.ReviewerName, map => map.MapFrom(x => x.Reviewer.FirstName +" "+ x.Reviewer.LastName));
+            #endregion
+
+            #region Product
+            CreateMap<ProductCreateDto, Product>();
             CreateMap<Product, ProductViewDto>()
                 .ForMember(x => x.Name, map => map.MapFrom(x => x.Name))
                 .ForMember(x => x.PaymentMethod, map => map.MapFrom(x => x.PaymentMethod))
@@ -31,6 +31,29 @@ namespace OnlineMarket.API.MappingProfiles
                 .ForMember(x => x.Stock, map => map.MapFrom(x => x.Stock))
                 .ForMember(x => x.Category, map => map.MapFrom(x => x.Category))
                 .ForMember(x => x.Seller, map => map.MapFrom(x => x.Seller));
+            #endregion
+
+            #region Product Review
+            CreateMap<ProductReviewCreateDto, ProductReview>();
+            CreateMap<ProductReview, ProductReviewViewDto>()
+                .ForMember(x => x.ReviewerName, map => map.MapFrom(x => x.Reviewer.FirstName +" "+ x.Reviewer.LastName));
+            #endregion
+
+            #region Images
+            CreateMap<Image, MediaViewDto>()
+				.ForMember(x => x.Link, map => map.MapFrom(x => x.Link))
+				.ForMember(x => x.MimeType, map => map.MapFrom(x => x.MimeType))
+				.ForMember(x => x.Type, map => map.MapFrom(x => x.Type));
+            #endregion
+
+            #region Delivery
+            CreateMap<DeliveryCreateDto, Delivery>();
+            #endregion
+
+            #region Transaction
+            CreateMap<TransactionCreateDto, Transaction>();
+            #endregion
+            
         }
     }
 }
