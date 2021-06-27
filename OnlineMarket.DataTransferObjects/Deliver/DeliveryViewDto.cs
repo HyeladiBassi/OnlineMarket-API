cb62ca559b623859e9e11497f8a380b639988ccb
HyeladiBassi <bassihyeladi@gmail.com>
2021-06-27 15:26:14 +0300

    Added city and address to dto

--- OnlineMarket.DataTransferObjects/Deliver/DeliveryViewDto.cs
+++ OnlineMarket.DataTransferObjects/Deliver/DeliveryViewDto.cs
@@ -3,8 +3,11 @@ namespace OnlineMarket.DataTransferObjects.Deliver
     public class DeliveryViewDto
     {
         public int Id { get; set; }
+        public string City { get; set; }
+        public string Area { get; set; }
         public string Region { get; set; }
         public string Address { get; set; }
+        public string PaymentMethod { get; set; }
         public string ExtraDetails { get; set; }
         public string Currency { get; set; }
         public double Price { get; set; }
